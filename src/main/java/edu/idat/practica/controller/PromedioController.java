55fd2d3fdb197df4beb92f0049d39ed55fa2f2c3
LuisPozo-06 <luispg132@gmail.com>
2025-03-13 21:55:17 -0500

    Segundo commit

--- src/main/java/edu/idat/practica/controller/PromedioController.java
+++ src/main/java/edu/idat/practica/controller/PromedioController.java
@@ -1,4 +1,39 @@
 package edu.idat.practica.controller;
 
+import edu.idat.practica.model.PromedioModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class PromedioController {
+
+    @GetMapping("/promedio")
+    public String formularioPromeido (Model model){
+    model.addAttribute("promediomodel", new PromedioModel());
+    return "promedio";
+    }
+@PostMapping ("/calcularPromedio")
+public String calcularPromedio (@ModelAttribute ("promediomodel")PromedioModel promedio,
+                                Model model) {
+    Double n1 = promedio.getN1();
+    Double n2 = promedio.getN2();
+    Double n3 = promedio.getN3();
+    Double ef = promedio.getEf();
+    Double resultado = (n1 * 0.04) + (n2 * 0.12) + (n3 * 0.24) + (ef * 0.60);
+    String resultadotxt = "";
+
+    if (resultado <= 13){
+        resultadotxt = "Desaprobado";
+
+    }
+    else {
+        resultadotxt = "Aprobado";
+    }
+    model.addAttribute("resultado", "El promedio es: " + resultado + ",te encuentras"+resultadotxt);
+    return "promedio";
+    }
+
 }
--- src/main/java/edu/idat/practica/model/PromedioModel.java
+++ src/main/java/edu/idat/practica/model/PromedioModel.java
@@ -1,4 +1,40 @@
 package edu.idat.practica.model;
 
 public class PromedioModel {
+    private Double n1;
+    private Double n2;
+    private Double n3;
+    private Double ef;
+
+    public Double getN1() {
+        return n1;
+    }
+
+    public void setN1(Double n1) {
+        this.n1 = n1;
+    }
+
+    public Double getN2() {
+        return n2;
+    }
+
+    public void setN2(Double n2) {
+        this.n2 = n2;
+    }
+
+    public Double getN3() {
+        return n3;
+    }
+
+    public void setN3(Double n3) {
+        this.n3 = n3;
+    }
+
+    public Double getEf() {
+        return ef;
+    }
+
+    public void setEf(Double ef) {
+        this.ef = ef;
+    }
 }
